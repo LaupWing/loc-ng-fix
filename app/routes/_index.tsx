9de2c96296e968bbc18eb4cdff7eb79612d36e44
laupwing <laupwing@gmail.com>
2025-02-08 19:46:54 +0100

    feat: comment out FeaturedCollection component for future reference

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -92,27 +92,27 @@ export default function Homepage() {
     )
 }
 
-function FeaturedCollection({
-    collection,
-}: {
-    collection: FeaturedCollectionFragment
-}) {
-    if (!collection) return null
-    const image = collection?.image
-    return (
-        <Link
-            className="featured-collection"
-            to={`/collections/${collection.handle}`}
-        >
-            {image && (
-                <div className="featured-collection-image">
-                    <Image data={image} sizes="100vw" />
-                </div>
-            )}
-            <h1>{collection.title}</h1>
-        </Link>
-    )
-}
+// function FeaturedCollection({
+//     collection,
+// }: {
+//     collection: FeaturedCollectionFragment
+// }) {
+//     if (!collection) return null
+//     const image = collection?.image
+//     return (
+//         <Link
+//             className="featured-collection"
+//             to={`/collections/${collection.handle}`}
+//         >
+//             {image && (
+//                 <div className="featured-collection-image">
+//                     <Image data={image} sizes="100vw" />
+//                 </div>
+//             )}
+//             <h1>{collection.title}</h1>
+//         </Link>
+//     )
+// }
 
 function HeroText() {
     return (
