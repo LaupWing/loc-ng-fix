2db234c7dc2b032c4b44af5b13db3027323d523b
laupwing <laupwing@gmail.com>
2025-02-08 18:33:31 +0100

    feat: format GraphQL queries for improved readability and consistency

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -143,20 +143,20 @@ const FEATURED_COLLECTION_QUERY = `#graphql
         id
         title
         image {
-        id
-        url
-        altText
-        width
-        height
+            id
+            url
+            altText
+            width
+            height
         }
         handle
     }
     query FeaturedCollection($country: CountryCode, $language: LanguageCode)
         @inContext(country: $country, language: $language) {
         collections(first: 1, sortKey: UPDATED_AT, reverse: true) {
-        nodes {
-            ...FeaturedCollection
-        }
+            nodes {
+                ...FeaturedCollection
+            }
         }
     }
 ` as const
@@ -167,27 +167,27 @@ const RECOMMENDED_PRODUCTS_QUERY = `#graphql
         title
         handle
         priceRange {
-        minVariantPrice {
-            amount
-            currencyCode
-        }
+            minVariantPrice {
+                amount
+                currencyCode
+            }
         }
         images(first: 1) {
-        nodes {
-            id
-            url
-            altText
-            width
-            height
-        }
+            nodes {
+                id
+                url
+                altText
+                width
+                height
+            }
         }
     }
     query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
         @inContext(country: $country, language: $language) {
         products(first: 4, sortKey: UPDATED_AT, reverse: true) {
-        nodes {
-            ...RecommendedProduct
-        }
+            nodes {
+                ...RecommendedProduct
+            }
         }
     }
 ` as const
