75c4864ba8840d73bfe7329fa994dcc59db02b8f
laupwing <laupwing@gmail.com>
2025-02-08 19:51:41 +0100

    feat: update RecommendedProducts component with new styling and layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -148,41 +148,43 @@ function RecommendedProducts({
     products: Promise<RecommendedProductsQuery | null>
 }) {
     return (
-        <div className="recommended-products">
-            <h2>Recommended Products</h2>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Await resolve={products}>
-                    {(response) => (
-                        <div className="recommended-products-grid">
-                            {response
-                                ? response.products.nodes.map((product) => (
-                                      <Link
-                                          key={product.id}
-                                          className="recommended-product"
-                                          to={`/products/${product.handle}`}
-                                      >
-                                          <Image
-                                              data={product.images.nodes[0]}
-                                              aspectRatio="1/1"
-                                              sizes="(min-width: 45em) 20vw, 50vw"
-                                          />
-                                          <h4>{product.title}</h4>
-                                          <small>
-                                              <Money
-                                                  data={
-                                                      product.priceRange
-                                                          .minVariantPrice
-                                                  }
+        <div className="bg-white">
+            <div className="custom-container py-8">
+                <h2>Recommended Products</h2>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Await resolve={products}>
+                        {(response) => (
+                            <div className="recommended-products-grid">
+                                {response
+                                    ? response.products.nodes.map((product) => (
+                                          <Link
+                                              key={product.id}
+                                              className="recommended-product"
+                                              to={`/products/${product.handle}`}
+                                          >
+                                              <Image
+                                                  data={product.images.nodes[0]}
+                                                  aspectRatio="1/1"
+                                                  sizes="(min-width: 45em) 20vw, 50vw"
                                               />
-                                          </small>
-                                      </Link>
-                                  ))
-                                : null}
-                        </div>
-                    )}
-                </Await>
-            </Suspense>
-            <br />
+                                              <h4>{product.title}</h4>
+                                              <small>
+                                                  <Money
+                                                      data={
+                                                          product.priceRange
+                                                              .minVariantPrice
+                                                      }
+                                                  />
+                                              </small>
+                                          </Link>
+                                      ))
+                                    : null}
+                            </div>
+                        )}
+                    </Await>
+                </Suspense>
+                <br />
+            </div>
         </div>
     )
 }
