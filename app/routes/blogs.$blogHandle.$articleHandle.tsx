c55bde7c7c0db79e7e31f69966290290755a651b
laupwing <laupwing@gmail.com>
2025-02-09 15:44:41 +0100

    refactor: clean up code formatting and improve readability in blog article route

--- app/routes/blogs.$blogHandle.$articleHandle.tsx
+++ app/routes/blogs.$blogHandle.$articleHandle.tsx
@@ -1,46 +1,46 @@
-import {defer, type LoaderFunctionArgs} from '@netlify/remix-runtime';
-import {useLoaderData, type MetaFunction} from '@remix-run/react';
-import {Image} from '@shopify/hydrogen';
+import { defer, type LoaderFunctionArgs } from "@netlify/remix-runtime"
+import { useLoaderData, type MetaFunction } from "@remix-run/react"
+import { Image } from "@shopify/hydrogen"
 
-export const meta: MetaFunction<typeof loader> = ({data}) => {
-  return [{title: `Hydrogen | ${data?.article.title ?? ''} article`}];
-};
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+    return [{ title: `Hydrogen | ${data?.article.title ?? ""} article` }]
+}
 
 export async function loader(args: LoaderFunctionArgs) {
-  // Start fetching non-critical data without blocking time to first byte
-  const deferredData = loadDeferredData(args);
+    // Start fetching non-critical data without blocking time to first byte
+    const deferredData = loadDeferredData(args)
 
-  // Await the critical data required to render initial state of the page
-  const criticalData = await loadCriticalData(args);
+    // Await the critical data required to render initial state of the page
+    const criticalData = await loadCriticalData(args)
 
-  return defer({...deferredData, ...criticalData});
+    return defer({ ...deferredData, ...criticalData })
 }
 
 /**
  * Load data necessary for rendering content above the fold. This is the critical data
  * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
  */
-async function loadCriticalData({context, params}: LoaderFunctionArgs) {
-  const {blogHandle, articleHandle} = params;
+async function loadCriticalData({ context, params }: LoaderFunctionArgs) {
+    const { blogHandle, articleHandle } = params
 
-  if (!articleHandle || !blogHandle) {
-    throw new Response('Not found', {status: 404});
-  }
+    if (!articleHandle || !blogHandle) {
+        throw new Response("Not found", { status: 404 })
+    }
 
-  const [{blog}] = await Promise.all([
-    context.storefront.query(ARTICLE_QUERY, {
-      variables: {blogHandle, articleHandle},
-    }),
-    // Add other queries here, so that they are loaded in parallel
-  ]);
+    const [{ blog }] = await Promise.all([
+        context.storefront.query(ARTICLE_QUERY, {
+            variables: { blogHandle, articleHandle },
+        }),
+        // Add other queries here, so that they are loaded in parallel
+    ])
 
-  if (!blog?.articleByHandle) {
-    throw new Response(null, {status: 404});
-  }
+    if (!blog?.articleByHandle) {
+        throw new Response(null, { status: 404 })
+    }
 
-  const article = blog.articleByHandle;
+    const article = blog.articleByHandle
 
-  return {article};
+    return { article }
 }
 
 /**
@@ -48,66 +48,75 @@ async function loadCriticalData({context, params}: LoaderFunctionArgs) {
  * fetched after the initial page load. If it's unavailable, the page should still 200.
  * Make sure to not throw any errors here, as it will cause the page to 500.
  */
-function loadDeferredData({context}: LoaderFunctionArgs) {
-  return {};
+function loadDeferredData({ context }: LoaderFunctionArgs) {
+    return {}
 }
 
 export default function Article() {
-  const {article} = useLoaderData<typeof loader>();
-  const {title, image, contentHtml, author} = article;
+    const { article } = useLoaderData<typeof loader>()
+    const { title, image, contentHtml, author } = article
 
-  const publishedDate = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(new Date(article.publishedAt));
+    const publishedDate = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(new Date(article.publishedAt))
 
-  return (
-    <div className="article">
-      <h1>
-        {title}
-        <div>
-          {publishedDate} &middot; {author?.name}
+    return (
+        <div className="bg-white items-center flex flex-col">
+            <small className="mt-4 text-neutral-500">
+                {publishedDate} &middot; {author?.name}
+            </small>
+            <h1 className="mx-auto text-4xl md:text-5xl font-bold pt-2 pb-6">
+                {title}
+            </h1>
+
+            {image && (
+                <Image
+                    className="custom-container mt-4 object-cover rounded-2xl"
+                    data={image}
+                    aspectRatio="16/9"
+                    sizes="90vw"
+                    loading="eager"
+                />
+            )}
+            <div
+                id="article-content"
+                dangerouslySetInnerHTML={{ __html: contentHtml }}
+                className="py-10 px-4 w-full max-w-5xl mx-auto grid gap-4"
+            />
         </div>
-      </h1>
-
-      {image && <Image data={image} sizes="90vw" loading="eager" />}
-      <div
-        dangerouslySetInnerHTML={{__html: contentHtml}}
-        className="article"
-      />
-    </div>
-  );
+    )
 }
 
 // NOTE: https://shopify.dev/docs/api/storefront/latest/objects/blog#field-blog-articlebyhandle
 const ARTICLE_QUERY = `#graphql
-  query Article(
-    $articleHandle: String!
-    $blogHandle: String!
-    $country: CountryCode
-    $language: LanguageCode
-  ) @inContext(language: $language, country: $country) {
-    blog(handle: $blogHandle) {
-      articleByHandle(handle: $articleHandle) {
-        title
-        contentHtml
-        publishedAt
-        author: authorV2 {
-          name
-        }
-        image {
-          id
-          altText
-          url
-          width
-          height
-        }
-        seo {
-          description
+    query Article(
+      $articleHandle: String!
+      $blogHandle: String!
+      $country: CountryCode
+      $language: LanguageCode
+    ) @inContext(language: $language, country: $country) {
+      blog(handle: $blogHandle) {
+        articleByHandle(handle: $articleHandle) {
           title
+          contentHtml
+          publishedAt
+          author: authorV2 {
+            name
+          }
+          image {
+            id
+            altText
+            url
+            width
+            height
+          }
+          seo {
+            description
+            title
+          }
         }
       }
     }
-  }
-` as const;
+` as const
