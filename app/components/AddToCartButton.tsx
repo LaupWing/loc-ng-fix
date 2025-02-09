26e46970815de13e89f9383c33fddf1466ae8856
laupwing <laupwing@gmail.com>
2025-02-09 10:27:44 +0100

    feat: enhance AddToCartButton component with improved styling and layout

--- app/components/AddToCartButton.tsx
+++ app/components/AddToCartButton.tsx
@@ -1,37 +1,42 @@
-import {type FetcherWithComponents} from '@remix-run/react';
-import {CartForm, type OptimisticCartLineInput} from '@shopify/hydrogen';
+import { type FetcherWithComponents } from "@remix-run/react"
+import { CartForm, type OptimisticCartLineInput } from "@shopify/hydrogen"
 
 export function AddToCartButton({
-  analytics,
-  children,
-  disabled,
-  lines,
-  onClick,
+    analytics,
+    children,
+    disabled,
+    lines,
+    onClick,
 }: {
-  analytics?: unknown;
-  children: React.ReactNode;
-  disabled?: boolean;
-  lines: Array<OptimisticCartLineInput>;
-  onClick?: () => void;
+    analytics?: unknown
+    children: React.ReactNode
+    disabled?: boolean
+    lines: Array<OptimisticCartLineInput>
+    onClick?: () => void
 }) {
-  return (
-    <CartForm route="/cart" inputs={{lines}} action={CartForm.ACTIONS.LinesAdd}>
-      {(fetcher: FetcherWithComponents<any>) => (
-        <>
-          <input
-            name="analytics"
-            type="hidden"
-            value={JSON.stringify(analytics)}
-          />
-          <button
-            type="submit"
-            onClick={onClick}
-            disabled={disabled ?? fetcher.state !== 'idle'}
-          >
-            {children}
-          </button>
-        </>
-      )}
-    </CartForm>
-  );
+    return (
+        <CartForm
+            route="/cart"
+            inputs={{ lines }}
+            action={CartForm.ACTIONS.LinesAdd}
+        >
+            {(fetcher: FetcherWithComponents<any>) => (
+                <>
+                    <input
+                        name="analytics"
+                        type="hidden"
+                        value={JSON.stringify(analytics)}
+                    />
+                    <button
+                        type="submit"
+                        onClick={onClick}
+                        disabled={disabled ?? fetcher.state !== "idle"}
+                        className=" text-center bg-yellow-300 font-bold text-sm uppercase py-3 rounded-full w-full"
+                    >
+                        {children}
+                    </button>
+                </>
+            )}
+        </CartForm>
+    )
 }
