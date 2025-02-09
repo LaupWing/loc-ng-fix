bb22e27e67cdddb5b228a2c5e4b2859be732f35c
laupwing <laupwing@gmail.com>
2025-02-09 17:29:19 +0100

    feat: add quantity selector to ProductForm for improved user interaction

--- app/components/ProductForm.tsx
+++ app/components/ProductForm.tsx
@@ -1,10 +1,17 @@
-import { Link } from "@remix-run/react"
-import { type VariantOption, VariantSelector } from "@shopify/hydrogen"
+import { FetcherWithComponents, Link } from "@remix-run/react"
+import {
+    CartForm,
+    OptimisticCartLineInput,
+    type VariantOption,
+    VariantSelector,
+} from "@shopify/hydrogen"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { useState } from "react"
 import type {
     ProductFragment,
     ProductVariantFragment,
 } from "storefrontapi.generated"
-import { AddToCartButton } from "~/components/AddToCartButton"
+// import { AddToCartButton } from "~/components/AddToCartButton"
 import { useAside } from "~/components/Aside"
 
 export function ProductForm({
@@ -17,8 +24,10 @@ export function ProductForm({
     variants: Array<ProductVariantFragment>
 }) {
     const { open } = useAside()
+    const [quantity, setQuantity] = useState(1)
+
     return (
-        <div className="product-form">
+        <div className="grid gap-4 max-w-md mx-auto w-full">
             <VariantSelector
                 handle={product.handle}
                 options={product.options.filter(
@@ -30,6 +39,26 @@ export function ProductForm({
                     <ProductOptions key={option.name} option={option} />
                 )}
             </VariantSelector>
+            <div className="flex items-center gap-4 bg-neutral-50/50 mr-auto border border-neutral-200 rounded-full p-2">
+                <button
+                    className="p-1.5 px-2 text-neutral-400"
+                    onClick={() => {
+                        if (quantity == 1) return
+                        setQuantity(quantity - 1)
+                    }}
+                >
+                    <ChevronLeft />
+                </button>
+                <div className="text-lg">{quantity}</div>
+                <button
+                    className="p-1.5 px-2 text-neutral-400"
+                    onClick={() => {
+                        setQuantity(quantity + 1)
+                    }}
+                >
+                    <ChevronRight />
+                </button>
+            </div>
             <AddToCartButton
                 disabled={!selectedVariant || !selectedVariant.availableForSale}
                 onClick={() => {
@@ -40,8 +69,7 @@ export function ProductForm({
                         ? [
                               {
                                   merchandiseId: selectedVariant.id,
-                                  quantity: 1,
-                                  selectedVariant,
+                                  quantity: quantity,
                               },
                           ]
                         : []
@@ -50,6 +78,46 @@ export function ProductForm({
                 {selectedVariant?.availableForSale ? "Add to cart" : "Sold out"}
             </AddToCartButton>
         </div>
+    )
+}
+
+function AddToCartButton({
+    analytics,
+    children,
+    disabled,
+    lines,
+    onClick,
+}: {
+    analytics?: unknown
+    children: React.ReactNode
+    disabled?: boolean
+    lines: Array<OptimisticCartLineInput>
+    onClick?: () => void
+}) {
+    return (
+        <CartForm
+            route="/cart"
+            inputs={{ lines }}
+            action={CartForm.ACTIONS.LinesAdd}
+        >
+            {(fetcher: FetcherWithComponents<any>) => (
+                <>
+                    <input
+                        name="analytics"
+                        type="hidden"
+                        value={JSON.stringify(analytics)}
+                    />
+                    <button
+                        type="submit"
+                        onClick={onClick}
+                        disabled={disabled ?? fetcher.state !== "idle"}
+                        className=" text-center bg-yellow-300 font-bold text-sm uppercase py-3 rounded-full w-full"
+                    >
+                        {children}
+                    </button>
+                </>
+            )}
+        </CartForm>
     )
 }
 
